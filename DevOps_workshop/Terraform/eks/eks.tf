162761257f554333510feb3a6444e8ab3727cb3e
runtiru <tirumalagadiya@gmail.com>
2023-12-14 17:24:21 +0530

    added EKS modules to terraform

--- DevOps_workshop/Terraform/eks/eks.tf
+++ DevOps_workshop/Terraform/eks/eks.tf
@@ -4,11 +4,15 @@ resource "aws_iam_role" "master" {
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
-  "Statement": {
-    "Effect": "Allow",
-    "Action": "sts:AssumeRole",
-    "Resource": "*"
-  }
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
 POLICY
 }
--- DevOps_workshop/Terraform/vpc/EC2.tf
+++ DevOps_workshop/Terraform/vpc/EC2.tf
@@ -140,6 +140,6 @@ module "sgs" {
 module "eks" {
   source     = "../eks"
   vpc_id     = aws_vpc.my_vpc.id
-  subnet_ids = [aws_subnet.my_pub_subnet_01.id, aws_subnet.my_pub_subnet_02.id]
+  subnet_ids = [aws_subnet.my_pub_subnet_01.id,aws_subnet.my_pub_subnet_02.id]
   sg_ids     = module.sgs.security_group_public
 }
