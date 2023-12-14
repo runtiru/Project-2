9a0392b7663ef8c7295d1345ae9e3835ec2b93b7
runtiru <tirumalagadiya@gmail.com>
2023-12-14 17:47:17 +0530

    added changes

--- DevOps_workshop/Terraform/sg_eks/sg.tf
+++ DevOps_workshop/Terraform/sg_eks/sg.tf
@@ -10,7 +10,14 @@ resource "aws_security_group" "worker_node_sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-
+  ingress {
+    description      = "ssh port"
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
   egress {
     from_port        = 0
     to_port          = 0
